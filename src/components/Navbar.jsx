43ef6a17c432f7aaccb6b8a1f6a7ae2ca54da3ec
Sharif H. Adal <mdadalbadsha2008@gmail.com>
2025-11-08 23:16:07 +0600

    Refactor Navbar component and update styles in index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link, Links, NavLink } from 'react-router'
 
 const Navbar = () => {
 
   const navLinks = (<>
-    <li><a>Item 1</a></li>
+    <li><NavLink to="/">Home</NavLink></li>
+    <li><NavLink to="/add-model">Add Model</NavLink></li>
+    <li><NavLink to="/view-models">View Models</NavLink></li>
   </>)
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar max-w-11/12 mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -19,7 +22,7 @@ const Navbar = () => {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to="/" className="text-xl font-medium">Model Stack</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -27,7 +30,22 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Login</a>
+        {/* <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><Link to="/model-purchase">Model Purchase</Link></li>
+            <li><Link to="/my-models">My Purchase</Link></li>
+          </ul>
+        </div> */}
       </div>
     </div>
   )
